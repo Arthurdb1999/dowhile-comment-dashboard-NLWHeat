b9a5cb5963b0126f890813876b341034d4f0f2dd
arthurdb1999 <arthurdb1999@gmail.com>
2021-10-19 23:25:22 -0300

    DONE: back-end

---
+++ server/src/controllers/GetLastThreeMessagesController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+import { GetLastThreeMessagesService } from '../services/GetLastThreeMessagesService'
+
+class GetLastThreeMessagesController {
+
+    async handle(request: Request, response: Response) {
+
+        const service = new GetLastThreeMessagesService()
+
+        try {
+            const result = await service.execute()
+
+            return response.json(result)
+        } catch (error) {
+            return response.status(400).json({ error: error.message })
+        }
+    }
+}
+
+export { GetLastThreeMessagesController }
